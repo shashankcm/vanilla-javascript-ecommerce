f4d2d94423c7ecc1fcf039ac80bf58fc8cbac088
Shashank Gupta <shashankcm90@gmail.com>
2019-08-03 22:35:46 -0400

    Completed Static UI

---
+++ app.js
@@ -0,0 +1,34 @@
+//variables
+cost cartBtn = document.querySelector('.cart-btn');
+cost closeCartBtn = document.querySelector('.close-cart');
+cost clearCartBtn = document.querySelector('.clear-cart');
+cost cartDOM = document.querySelector('.cart');
+cost cartOverlay = document.querySelector('.cart-overlay');
+cost cartItems = document.querySelector('.cart-items');
+cost cartTotal = document.querySelector('.cart-total');
+cost cartContent = document.querySelector('.cart-content');
+cost productsDOM = document.querySelector('.products-center');
+
+//cart
+let cart = [];
+
+//getting the products
+class Products {
+
+}
+
+// display products
+class UI {
+
+}
+
+//local storage
+
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+  const ui = new UI();
+  const products = new Products();
+})
